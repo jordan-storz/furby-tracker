77c6940b4b8393212788804b8c1b35517c0546da
jordan-storz <jordan.storz.1@gmail.com>
2016-12-14 14:12:31 -0700

    Make dotenv silently fail for heroku

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 const databaseName = 'furby_tracker';
-require('dotenv').config();
+require('dotenv').config({silent: true});
 
 module.exports = {
   development: {
@@ -14,7 +14,13 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL + '?ssl=true',
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
   },
   test: {
     client: 'postgresql',
